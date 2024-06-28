65cb3a6c4bf2695041200b860bb579cbf401797c
neliz3 <eliza.nadrega14@gmail.com>
2024-06-28 11:34:46 +0300

    user added

--- create_user.sql
+++ create_user.sql
@@ -1,9 +1,16 @@
 USE beauty_agency;
 
--- CREATE USER
+-- Administrator permissions
+CREATE USER 'administrator'@'localhost' IDENTIFIED WITH mysql_native_password BY 'administrator123';
+GRANT ALL PRIVILEGES ON beauty_agency.* TO 'administrator'@'localhost';
 
-CREATE USER 'schedule_reader1'@'localhost' IDENTIFIED BY '123456Aa@';
+-- Support permissions
+CREATE USER 'support'@'localhost' IDENTIFIED WITH mysql_native_password BY 'support132';
+GRANT INSERT, UPDATE, SELECT, RELOAD ON beauty_agency.* TO 'support'@'localhost';
 
-GRANT SELECT ON schedules TO 'schedule_reader1'@'localhost';
+-- Manager permissions
+CREATE USER 'manager'@'localhost' IDENTIFIED WITH mysql_native_password BY 'manager123';
+GRANT SELECT ON beauty_agency.* TO 'manager'@'localhost';
 
-SHOW GRANTS FOR 'schedule_reader1'@'localhost';
+-- reload changes
+FLUSH PRIVILEGES;
